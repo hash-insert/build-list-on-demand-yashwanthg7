7340eb5ebeaeef7c4678ad3f807ad1b79d94ece4
yashwanthg7 <yashwanth18183@gmail.com>
2023-03-08 18:55:10 +0530

    ShowlessFunctionality

--- build-lists.js
+++ build-lists.js
@@ -6,26 +6,45 @@ const addListColumn = (start) => {
     const listColDiv = document.createElement("DIV");
     listColDiv.setAttribute("class", "list-col");
     const ulElem = document.createElement("UL");
-    for (let i=0; i< numsPerList; i++) {
+    for (let i = 0; i < numsPerList; i++) {
         const lielem = document.createElement("LI");
-        let liContent = document.createTextNode(i+start);
+        let liContent = document.createTextNode(i + start);
         lielem.appendChild(liContent)
         ulElem.appendChild(lielem)
     }
     listColDiv.appendChild(ulElem);
-    listDiv.appendChild(listColDiv);    
+    listDiv.appendChild(listColDiv);
+
 }
 
 let i = 1;
 
 const onButtonClick = () => {
-    i = i + numsPerList
-    addListColumn(i);
+    i = i + numsPerList;
+    const listCols = document.querySelectorAll(".list-col");
+    listCols.forEach(col => col.remove());
+    let start = 1;
+    while (start <= i) {
+        addListColumn(start);
+        start += numsPerList;
+    }
 }
 
+const onLessButtonClick = () => {
+    if (i > numsPerList) {
+        i = i - numsPerList;
+        const listCols = document.querySelectorAll(".list-col");
+        listCols.forEach(col => col.remove());
+        // for (let start = 1; start <= i; start += numsPerList) {
+        //     addListColumn(start); // Recreate all list columns up to the new value of "i"
+        // }
+        let start = 1;
+        while (start <= i) {
+            addListColumn(start);
+            start += numsPerList;
+        }
+    }
+}
+
+
 addListColumn(i);
-
-
-
-
-
